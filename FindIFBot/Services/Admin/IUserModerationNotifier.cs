d5cae4b9ae25814999a14a44e44ab91c0a4cfb7e
Oleksandr Shchur <71557324+OleksandrShchur@users.noreply.github.com>
2026-07-15 22:16:25 +0300

    F-44 - add request id to all user messages (#43)
    * add request id to every admin action response
    
    * cover by tests
    
    * fix failed tests

--- FindIFBot/Services/Admin/IUserModerationNotifier.cs
+++ FindIFBot/Services/Admin/IUserModerationNotifier.cs
@@ -3,7 +3,7 @@ namespace FindIFBot.Services.Admin
     public interface IUserModerationNotifier
     {
         Task NotifySubmittedAsync(long chatId, int requestId);
-        Task NotifyPublishedAsync(long userId, string channelLink);
+        Task NotifyPublishedAsync(long userId, string channelLink, int requestId);
         Task NotifyRejectedAsync(long userId, int messageId);
         Task NotifyDuplicateAsync(long userId, int messageId);
         Task NotifyAdvertisementAsync(long userId, int messageId);
